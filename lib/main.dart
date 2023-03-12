8e9f82f7fe61fd4e934ab68a6592800189653d83
prostreet1212@gmail.com <ktndlf,tcfyu2020>
2023-03-12 12:17:35 +0300

    map

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_18/screens/my_home_page.dart';
 import 'package:google_maps_18/widgets/map_sample.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -12,13 +15,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    if(defaultTargetPlatform==TargetPlatform.android){
+      AndroidGoogleMapsFlutter.useAndroidViewSurface=true;
+    }
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      //home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      home: MyHomePage(title: 'Google Maps',),
+      home: MyHomePage(title: 'гугл',),
+      //home: MapSample(),
     );
   }
 }
--- lib/screens/my_home_page.dart
+++ lib/screens/my_home_page.dart
@@ -1,8 +1,11 @@
 
 
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:google_maps_18/widgets/map_sample.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
+
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -14,10 +17,36 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  static const CameraPosition _kGooglePlex = CameraPosition(
+  final Completer<GoogleMapController> _controller =
+  Completer<GoogleMapController>();
+
+    CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
   );
+
+    LocationData? currentLocation;//=LocationData.fromMap({l});
+
+void getCurrentLocation(){
+  Location location=Location();
+  location.getLocation().then((location1) {
+    currentLocation=location1;
+  });
+  location.onLocationChanged.listen((newLocation) {
+    currentLocation=newLocation;
+    setState(() {
+
+    });
+  });
+}
+
+
+@override
+  void initState() {
+  getCurrentLocation();
+    super.initState();
+
+  }
 
 
 
@@ -30,19 +59,47 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Stack(
         children: [
           GoogleMap(
-            mapType: MapType.normal,
-            initialCameraPosition: _kGooglePlex,
-            myLocationButtonEnabled: true,),
-          Align(
-            alignment: Alignment.centerLeft,
+            mapType: MapType.hybrid,
+            initialCameraPosition: CameraPosition(
+              target: LatLng(currentLocation!.latitude!, currentLocation!.longitude!),
+              zoom: 14.4746,
+            ),
+            myLocationButtonEnabled: true,
+          zoomControlsEnabled: false,
+          onMapCreated: (controller){
+              _controller.complete(controller);
+          },
+            markers: {
+              Marker(markerId: MarkerId('source'),
+              position: LatLng(currentLocation!.latitude!, currentLocation!.longitude!))
+            },
+         ),
+          /*Flexible(
+              child: Align(
+            alignment: Alignment.bottomCenter,
             child: Slider(
               min: 0,
               max: 100,
               value: 10,
               onChanged: (double value) {  },
             ),
-          )
+          ),
+          )*/
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: ()async {
+        /*  Position i= await _determinePosition();
+          print(i.latitude.toString());
+          _kGooglePlex=CameraPosition(
+            target: LatLng(i.latitude,i.longitude),
+            zoom: 14.4746,
+          );
+          final GoogleMapController controller = await _controller.future;
+          controller.animateCamera(CameraUpdate.newCameraPosition(_kGooglePlex,),);*/
+        },
+
       ),
     );
   }
