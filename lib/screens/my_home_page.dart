09a02f945dea045134194bed7e241f8727bb8cb0
prostreet1212 <prostreet1212@gmail.com>
2023-03-13 17:06:03 +0300

    maps

--- lib/screens/my_home_page.dart
+++ lib/screens/my_home_page.dart
@@ -20,20 +20,48 @@ class _MyHomePageState extends State<MyHomePage> {
   final Completer<GoogleMapController> _controller =
   Completer<GoogleMapController>();
 
-    CameraPosition _kGooglePlex = CameraPosition(
+    /*CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
-  );
+  );*/
 
-    LocationData? currentLocation;//=LocationData.fromMap({l});
+    LocationData? currentLocation=LocationData.fromMap({ "latitude": 37.42796133580664,
+      "longitude":-122.085749655962 });//=LocationData.fromMap({l});
 
-void getCurrentLocation(){
+Future<void> getCurrentLocation() async {
   Location location=Location();
+  final GoogleMapController controller = await _controller.future;
+
+  bool _serviceEnabled;
+  PermissionStatus _permissionGranted;
+
+  _serviceEnabled = await location.serviceEnabled();
+  if (!_serviceEnabled) {
+    _serviceEnabled = await location.requestService();
+    if (!_serviceEnabled) {
+      return;
+    }
+  }
+
+  _permissionGranted = await location.hasPermission();
+  if (_permissionGranted == PermissionStatus.denied) {
+    _permissionGranted = await location.requestPermission();
+    if (_permissionGranted != PermissionStatus.granted) {
+      return;
+    }
+  }
+
   location.getLocation().then((location1) {
     currentLocation=location1;
   });
-  location.onLocationChanged.listen((newLocation) {
+
+  location.onLocationChanged.listen((newLocation)  {
     currentLocation=newLocation;
+
+    controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+        target: LatLng(currentLocation!.latitude!, currentLocation!.longitude!),
+        zoom: 14.4746
+    ),),);
     setState(() {
 
     });
@@ -52,8 +80,9 @@ void getCurrentLocation(){
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-     /* appBar: AppBar(
+    return SafeArea(
+        child: Scaffold(
+      /* appBar: AppBar(
         title: Text(widget.title),
       ),*/
       body: Stack(
@@ -65,15 +94,15 @@ void getCurrentLocation(){
               zoom: 14.4746,
             ),
             myLocationButtonEnabled: true,
-          zoomControlsEnabled: false,
-          onMapCreated: (controller){
+            zoomControlsEnabled: false,
+            onMapCreated: (controller){
               _controller.complete(controller);
-          },
+            },
             markers: {
               Marker(markerId: MarkerId('source'),
-              position: LatLng(currentLocation!.latitude!, currentLocation!.longitude!))
+                  position: LatLng(currentLocation!.latitude!, currentLocation!.longitude!))
             },
-         ),
+          ),
           /*Flexible(
               child: Align(
             alignment: Alignment.bottomCenter,
@@ -90,7 +119,7 @@ void getCurrentLocation(){
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: ()async {
-        /*  Position i= await _determinePosition();
+          /*  Position i= await _determinePosition();
           print(i.latitude.toString());
           _kGooglePlex=CameraPosition(
             target: LatLng(i.latitude,i.longitude),
@@ -101,6 +130,7 @@ void getCurrentLocation(){
         },
 
       ),
+    ),
     );
   }
 }
