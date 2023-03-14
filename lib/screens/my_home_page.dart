ad95cd1b4042e17009ba80b9b0798985d8fb8241
prostreet1212 <prostreet1212@gmail.com>
2023-03-14 22:55:02 +0300

    maps

--- lib/screens/my_home_page.dart
+++ lib/screens/my_home_page.dart
@@ -1,14 +1,12 @@
-
-
 import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 
-
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
+
   final String title;
 
   @override
@@ -16,94 +14,108 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   final Completer<GoogleMapController> _controller =
-  Completer<GoogleMapController>();
+      Completer<GoogleMapController>();
 
-    /*CameraPosition _kGooglePlex = CameraPosition(
+  /*CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
   );*/
 
-    LocationData? currentLocation=LocationData.fromMap({ "latitude": 37.42796133580664,
-      "longitude":-122.085749655962 });//=LocationData.fromMap({l});
+  LocationData? currentLocation = LocationData.fromMap({
+    "latitude": 37.42796133580664,
+    "longitude": -122.085749655962
+  }); //=LocationData.fromMap({l});
 
-Future<void> getCurrentLocation() async {
-  Location location=Location();
-  final GoogleMapController controller = await _controller.future;
+  Future<void> getCurrentLocation() async {
+    Location location = Location();
+    final GoogleMapController controller = await _controller.future;
 
-  bool _serviceEnabled;
-  PermissionStatus _permissionGranted;
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
 
-  _serviceEnabled = await location.serviceEnabled();
-  if (!_serviceEnabled) {
-    _serviceEnabled = await location.requestService();
+    _serviceEnabled = await location.serviceEnabled();
     if (!_serviceEnabled) {
-      return;
+      _serviceEnabled = await location.requestService();
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content:
+              Text('Включите геолокацию для определения вашего местоположения'),
+        ),
+      );
+      if (!_serviceEnabled) {
+        return;
+      }
     }
-  }
 
-  _permissionGranted = await location.hasPermission();
-  if (_permissionGranted == PermissionStatus.denied) {
-    _permissionGranted = await location.requestPermission();
-    if (_permissionGranted != PermissionStatus.granted) {
-      return;
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content:
+              Text('Включите разрешение "Местоположение"'),
+        ),
+      );
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
     }
-  }
 
-  location.getLocation().then((location1) {
-    currentLocation=location1;
-  });
-
-  location.onLocationChanged.listen((newLocation)  {
-    currentLocation=newLocation;
-
-    controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
-        target: LatLng(currentLocation!.latitude!, currentLocation!.longitude!),
-        zoom: 14.4746
-    ),),);
-    setState(() {
-
+    location.getLocation().then((location1) {
+      currentLocation = location1;
     });
-  });
-}
 
+    location.onLocationChanged.listen((newLocation) {
+      currentLocation = newLocation;
 
-@override
-  void initState() {
-  getCurrentLocation();
-    super.initState();
-
+      controller.animateCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(
+              target: LatLng(
+                  currentLocation!.latitude!, currentLocation!.longitude!),
+              zoom: 14.4746),
+        ),
+      );
+      setState(() {});
+    });
   }
 
-
+  @override
+  void initState() {
+    getCurrentLocation();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-      /* appBar: AppBar(
+      child: Scaffold(
+        /* appBar: AppBar(
         title: Text(widget.title),
       ),*/
-      body: Stack(
-        children: [
-          GoogleMap(
-            mapType: MapType.hybrid,
-            initialCameraPosition: CameraPosition(
-              target: LatLng(currentLocation!.latitude!, currentLocation!.longitude!),
-              zoom: 14.4746,
+        body: Stack(
+          children: [
+            GoogleMap(
+              mapType: MapType.hybrid,
+              initialCameraPosition: CameraPosition(
+                target: LatLng(
+                    currentLocation!.latitude!, currentLocation!.longitude!),
+                zoom: 14.4746,
+              ),
+              myLocationButtonEnabled: true,
+              zoomControlsEnabled: false,
+              onMapCreated: (controller) {
+                _controller.complete(controller);
+              },
+              markers: {
+                Marker(
+                    markerId: MarkerId('source'),
+                    position: LatLng(currentLocation!.latitude!,
+                        currentLocation!.longitude!))
+              },
             ),
-            myLocationButtonEnabled: true,
-            zoomControlsEnabled: false,
-            onMapCreated: (controller){
-              _controller.complete(controller);
-            },
-            markers: {
-              Marker(markerId: MarkerId('source'),
-                  position: LatLng(currentLocation!.latitude!, currentLocation!.longitude!))
-            },
-          ),
-          /*Flexible(
+            /*Flexible(
               child: Align(
             alignment: Alignment.bottomCenter,
             child: Slider(
@@ -114,12 +126,12 @@ Future<void> getCurrentLocation() async {
             ),
           ),
           )*/
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: ()async {
-          /*  Position i= await _determinePosition();
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () async {
+            /*  Position i= await _determinePosition();
           print(i.latitude.toString());
           _kGooglePlex=CameraPosition(
             target: LatLng(i.latitude,i.longitude),
@@ -127,10 +139,9 @@ Future<void> getCurrentLocation() async {
           );
           final GoogleMapController controller = await _controller.future;
           controller.animateCamera(CameraUpdate.newCameraPosition(_kGooglePlex,),);*/
-        },
-
+          },
+        ),
       ),
-    ),
     );
   }
 }
